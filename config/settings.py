3a42d77152f5eebdc0f56af4f30a44562f4ac024
abijith-ab1 <abijithkp93@gmail.com>
2025-10-16 13:19:58 +0530

    changed database settings

--- config/settings.py
+++ config/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_DB", "test_db"),
         "USER": os.environ.get("POSTGRES_USER", "postgres"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
-        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "HOST": os.environ.get("POSTGRES_HOST", "postgres"),
         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
