135f65f7687fa4aee8ddf30e11dfb39884eb03c5
abijith-ab1 <abijithkp93@gmail.com>
2025-10-17 09:10:47 +0530

    Enhance CI workflow to ensure proper Postgres readiness check and define service ports

--- config/settings.py
+++ config/settings.py
@@ -66,11 +66,10 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_DB", "test_db"),
         "USER": os.environ.get("POSTGRES_USER", "postgres"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
-        "HOST": os.environ.get("POSTGRES_HOST", "postgres"),
+        "HOST": os.environ.get("POSTGRES_HOST", "postgres"),  # important!
         "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
-
 # Caching via Redis
 REDIS_URL = os.getenv('REDIS_URL', 'redis://localhost:6379/0')
 CACHES = {
